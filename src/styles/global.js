01b2a022ab3ed5021acc3418ee3dbdd92025f2e4
Danilo <danilojssantos1@gmail.com>
2024-04-01 15:13:09 -0300

    implementado font personalizadas

--- src/styles/global.js
+++ src/styles/global.js
@@ -16,6 +16,14 @@ export default createGlobalStyle`
     body{
         background-color: ${({ theme })=> theme.COLORS.BACKGROUND_800};
         color: ${({ theme })=> theme.COLORS.WHITE};
+
+        -webkit-font-smoothing: antialiased; 
+    }
+
+    body, input, button, textarea {
+        font-family: "Roboto Slab", serif;
+        font-size: 1.6rem;
+        outline: none;
     }
 
     a {
