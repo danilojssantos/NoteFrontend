9ce39b99abd10be568b2d53027fe6d5197f49e9c
Danilo <danilojssantos1@gmail.com>
2024-04-01 14:46:58 -0300

    iniciando css global

---
+++ src/styles/global.js
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    *{
+        margin:0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    :root{
+
+        font-size: 62.5%;
+        }
+
+
+    body{
+        background-color: ${({ theme })=> theme.COLORS.BACKGROUND_800};
+        color: ${({ theme })=> theme.COLORS.WHITE};
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    buttom, a {
+        cursor: pointer;
+        transition: filter 0.2s;
+    }
+
+    buttom:hover, a:hover {
+        filter: brightness(0.9)
+    }
+
+
+`
+
