927ab524de2aa4fb2cf72a2503572a1542047549
Danilo <danilojssantos1@gmail.com>
2024-04-01 16:02:14 -0300

    componente Button

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,9 +1,11 @@
 import {Container} from "./styles"
+import { Button } from "../../components/Button"
 export  function Details(){
   
   return(
     <Container>
     <h1>Ola mundo</h1>
+    <Button/>
     </Container>
   )
 
