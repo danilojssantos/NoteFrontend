78c4fc92953e0f0d6111fd036be0f318036ee126
Danilo <danilojssantos1@gmail.com>
2024-04-03 11:32:23 -0300

    botao criar nota

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,5 @@
+import {FiPlus} from 'react-icons/fi';
+
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
 import {Header} from '../../components/Header'
 import {ButtonText} from '../../components/ButtonText'
@@ -22,10 +24,11 @@ export function Home(){
 
             <Content>
 
-            </Content>
+            </Content> 
 
             <NewNote>
-
+                <FiPlus/>
+                Criar nota
             </NewNote>
         </Container>
 
