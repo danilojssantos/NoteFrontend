7e23512ca4e7c17ca53751c1cd2e671105443646
Danilo <danilojssantos1@gmail.com>
2024-04-03 17:03:02 -0300

    formulario da pagina de login

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -1,9 +1,38 @@
-import { Container } from './styles'
+import { FiMail, FiLock } from 'react-icons/fi'
+//importação de componetes
+import { Input} from '../../components/Input'
+import {Button} from '../../components/Button'
+import { Container, Form } from './styles'
 
 
 export function Signin(){
     return(
         <Container>
+
+            <Form>
+                <h1>Notes</h1>
+                <p>Aplicação para salvar e gerenciar seus links uteis</p>
+                <h2>Faça seu Login</h2>
+
+                <Input 
+                 placeholder="E-mail"
+                 type="text"
+                 icon={FiMail}    
+                />
+
+                <Input 
+                 placeholder="Senha"
+                 type="password"
+                 icon={FiLock}    
+                />
+
+                <Button title="Entrar"/>
+
+                <a href='#'>
+                    Criar Conta
+                </a>    
+                
+            </Form>
 
         </Container>
     )
