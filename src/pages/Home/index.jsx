8fae28772e0dc796f4527bc659acf7b8b2c1a07e
Danilo <danilojssantos1@gmail.com>
2024-04-03 15:00:56 -0300

    componente Notes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,6 +3,8 @@ import {FiPlus, FiSearch} from 'react-icons/fi';
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
 import {Header} from '../../components/Header'
 import { Input } from '../../components/Input';
+import {Note} from '../../components/Note'
+import {Section} from '../../components/Section'
 import {ButtonText} from '../../components/ButtonText'
 export function Home(){
     return(
@@ -26,6 +28,17 @@ export function Home(){
             </Search>
 
             <Content>
+                <Section title="Minhas notas">
+                    <Note data={{
+                        title:'React', 
+                        tags:[
+                            {id:'1', name: 'react'},
+                            {id:'2', name: 'node' }
+                        ]
+                    }}
+                        />
+
+                </Section>
 
             </Content> 
 
