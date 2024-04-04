2d70d3991339a6ad709b066e6129be11fe3451b4
Danilo <danilojssantos1@gmail.com>
2024-04-04 10:17:28 -0300

    Pagina de Perfil formulario

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,50 @@
+import { Container , Form} from "./styles";
+import{FiArrowLeft,FiUser, FiMail,FiLock}from "react-icons/fi"
+
+import {Input} from "../../components/Input"
+import {Button} from "../../components/Button"
+
+
+
+export function Profile(){
+    return(
+        <Container>
+            <header>
+                <a href="/">
+                    <FiArrowLeft/>
+                </a>
+            </header>
+
+            <Form>
+                <Input 
+                    placeholder="Nome"
+                    type="text"
+                    icon={FiUser}  
+                />
+
+                <Input 
+                    placeholder="E-mail"
+                    type="text"
+                    icon={FiMail}  
+                /> 
+
+                 <Input 
+                    placeholder="senha atual"
+                    type="password"
+                    icon={FiLock}  
+                />   
+
+                 <Input 
+                    placeholder="nova senha"
+                    type="password"
+                    icon={FiLock}  
+                />    
+
+                <Button title="Salvar"/>
+                     
+
+            </Form>
+        </Container>
+    )
+}
+
---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,47 @@
+import { FiMail, FiLock, FiUser } from 'react-icons/fi'
+//importação de componetes
+import { Input} from '../../components/Input'
+import {Button} from '../../components/Button'
+import { Container, Form ,Background} from './styles'
+
+
+export function SignUp(){
+    return(
+        <Container>
+            <Background/>
+            <Form>
+                <h1>Notes</h1>
+                <p>Aplicação para salvar e gerenciar seus links uteis</p>
+                <h2>Crie sua conta</h2>
+                <Input 
+                 placeholder="Nome"
+                 type="text"
+                 icon={FiUser}    
+                />
+
+
+                <Input 
+                 placeholder="E-mail"
+                 type="text"
+                 icon={FiMail}    
+                />
+
+                <Input 
+                 placeholder="Senha"
+                 type="password"
+                 icon={FiLock}    
+                />
+
+                <Button title="Cadastrar"/>
+
+                <a href='#'>
+                    Volta para o Login
+                </a>    
+                
+            </Form>
+
+          
+
+        </Container>
+    )
+}
