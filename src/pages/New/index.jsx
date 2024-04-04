4c471d7f4413da1b50b6734f7847ca76a4356178
Danilo <danilojssantos1@gmail.com>
2024-04-04 11:37:55 -0300

    header pagina Nova nota

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,10 +1,22 @@
 import {Header} from '../../components/Header'
-import { Container} from './styles'
+import { Container , Form} from './styles'
+import {Input} from '../../components/Input'
 
 export function New(){
     return(
         <Container>
             <Header/>
+            <main>
+                <Form>
+                    <header>
+                        <h1>Criar nota </h1>
+                        <a href='/'>voltar</a>
+                    </header>
+                    <Input 
+                        placeholder="Titulo"
+                    />
+                </Form>
+            </main>
         </Container>
     )
 }
--- src/pages/New/styles.js
+++ src/pages/New/styles.js
@@ -12,3 +12,24 @@ export const Container = styled.div`
     "content";
 
 `;
+
+export const Form = styled.form`
+    max-width: 55rem;
+    margin: 3.8rem auto;
+
+    >header{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        margin-bottom: 3.6rem;
+
+        a{
+            font-size: 2.0rem;
+            color: ${({theme })=> theme.COLORS.GRAY_100}
+        }
+    }
+
+
+
+`;
