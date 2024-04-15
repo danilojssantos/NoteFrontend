b8343044ff7832f4ba6bee3cb4f8fec0e133b6d2
Danilo <danilojssantos1@gmail.com>
2024-04-15 17:13:16 -0300

    remover link

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -16,6 +16,11 @@ export function New(){
         setLinks(prevState =>[...prevState, newLink]);
         setNewLink("");
     }
+
+    function handleRemoveLink(deleted){
+        setLinks(prevState => prevState.filter(link => link !== deleted))
+
+    }
     return(
         <Container>
             <Header/>
@@ -38,7 +43,7 @@ export function New(){
                             <NoteItem 
                             key={String(index)}
                             value={link}
-                            onClick={handleAddLink}
+                            onClick={() => handleRemoveLink(link)}
                             />
 
 
